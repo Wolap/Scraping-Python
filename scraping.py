56235c89a6421530ee58457119d8d37a0fa615a6
Wolap <wolap.wolap@gmail.com>
2024-10-23 16:14:55 +0200

    :art: UPDATE code

--- scraping.py
+++ scraping.py
@@ -53,84 +53,88 @@ for category in categories:
     categories_names.append(category.find('a').text.strip().replace(' ', '_'))
 
 # Get all categories links and looks for them
-for i in range (len(categories_links)):
-    if categories_links[i] == "index.html":
-        i += 1
-    
-    url =  "https://books.toscrape.com/catalogue/category" + categories_links[i]
-    soup = fetch_soup(url)
+def scrap_books():
 
-    csv_path = os.path.join('csv', categories_names[i] + '.csv')
-    
-    # Open CSV file
-    with open(csv_path, 'w', newline='', encoding='utf-8') as fichier_csv:
-        writter = csv.writer(fichier_csv)
-        writter.writerow(['Titre', 'UPC', 'Prix TTC', 'Prix HT', 'Stock', 'Description', 'Categorie', 'Note', 'URL of image'])
+    for i in range (len(categories_links)):
+        if categories_links[i] == "index.html":
+            i += 1
+        
+        url =  "https://books.toscrape.com/catalogue/category" + categories_links[i]
+        soup = fetch_soup(url)
 
-    title_page = soup.find('h1').text
-    print("Titre Page ", title_page)
-
-    # Store pages links of a cathegory in a list
-    books_links = []
-    books = soup.find_all('h3')
-    for book in books:
-        books_links.append(book.find('a')['href'].replace('../../..', 'https://books.toscrape.com/catalogue'))
-
-    # Get books information from each page
-    for lien in books_links:
-        response = requests.get(lien)
-        soup = BeautifulSoup(response.text, "html.parser")
-
-        # UPC
-        universal_product_code = soup.find('table').find_all('tr')[0].text.split('UPC')[1]
-        # Titles
-        title = soup.find('h1').text.encode('utf-8').decode('utf-8')
-        clean_title = clean_filename(title)
-        # Price with taxes
-        try:
-            price_including_tax = soup.find('table').find_all('tr')[3].text.split('£')[1]
-        except:
-            price_including_tax = "n/a"
-        # Price without taxes
-        try:
-            price_excluding_tax = soup.find('table').find_all('tr')[2].text.split('£')[1]
-        except:
-            price_excluding_tax = "n/a"
-        # Stock
-        number_available = soup.find('table').find_all('tr')[5].text.split('(')[1].split(' available')[0]
-        # Description
-        try:
-            product_description = soup.find_all('p')[3].text
-        except:
-            product_description = "n/a"
-        # Category
-        category = soup.find_all('a')[3].text
-        # Review rating
-        try:
-            review_rating = soup.find('p', class_='star-rating')['class'][1]
-        except:
-            review_rating = "n/a"
-
-        # Creat folder for each category of images
-        if not os.path.exists('images/' + categories_names[i]):
-            os.makedirs('images/' + categories_names[i])
-        else:
-            pass
-            
-        # Image URL
-        image_url = soup.find('img')['src'].replace('../..', 'https://books.toscrape.com')
-        # Get image
-        image = requests.get(image_url)
-
-        # Check if image is valid
-        if image.status_code == 200:
-            # Save image
-            with open(f"images/{categories_names[i]}/{clean_title}.jpg", 'wb') as file:
-                file.write(image.content)
-        else:
-            print(f"Erreur lors du téléchargement de l'image pour le livre: {title}")
-
-        # Write data in CSV file
-        with open(csv_path, 'a', newline='', encoding='utf-8') as fichier_csv:
+        csv_path = os.path.join('csv', categories_names[i] + '.csv')
+        
+        # Open CSV file
+        with open(csv_path, 'w', newline='', encoding='utf-8') as fichier_csv:
             writter = csv.writer(fichier_csv)
-            writter.writerow([title, universal_product_code, price_including_tax, price_excluding_tax, number_available, product_description, category, review_rating, image_url])
+            writter.writerow(['Titre', 'UPC', 'Prix TTC', 'Prix HT', 'Stock', 'Description', 'Categorie', 'Note', 'URL of image'])
+
+        title_page = soup.find('h1').text
+        print("Titre Page ", title_page)
+
+        # Store pages links of a cathegory in a list
+        books_links = []
+        books = soup.find_all('h3')
+        for book in books:
+            books_links.append(book.find('a')['href'].replace('../../..', 'https://books.toscrape.com/catalogue'))
+
+        # Get books information from each page
+        for lien in books_links:
+            response = requests.get(lien)
+            soup = BeautifulSoup(response.text, "html.parser")
+
+            # UPC
+            universal_product_code = soup.find('table').find_all('tr')[0].text.split('UPC')[1]
+            # Titles
+            title = soup.find('h1').text.encode('utf-8').decode('utf-8')
+            clean_title = clean_filename(title)
+            # Price with taxes
+            try:
+                price_including_tax = soup.find('table').find_all('tr')[3].text.split('£')[1]
+            except:
+                price_including_tax = "n/a"
+            # Price without taxes
+            try:
+                price_excluding_tax = soup.find('table').find_all('tr')[2].text.split('£')[1]
+            except:
+                price_excluding_tax = "n/a"
+            # Stock
+            number_available = soup.find('table').find_all('tr')[5].text.split('(')[1].split(' available')[0]
+            # Description
+            try:
+                product_description = soup.find_all('p')[3].text
+            except:
+                product_description = "n/a"
+            # Category
+            category = soup.find_all('a')[3].text
+            # Review rating
+            try:
+                review_rating = soup.find('p', class_='star-rating')['class'][1]
+            except:
+                review_rating = "n/a"
+
+            # Creat folder for each category of images
+            if not os.path.exists('images/' + categories_names[i]):
+                os.makedirs('images/' + categories_names[i])
+            else:
+                pass
+                
+            # Image URL
+            image_url = soup.find('img')['src'].replace('../..', 'https://books.toscrape.com')
+            # Get image
+            image = requests.get(image_url)
+
+            # Check if image is valid
+            if image.status_code == 200:
+                # Save image
+                with open(f"images/{categories_names[i]}/{clean_title}.jpg", 'wb') as file:
+                    file.write(image.content)
+            else:
+                print(f"Erreur lors du téléchargement de l'image pour le livre: {title}")
+
+            # Write data in CSV file
+            with open(csv_path, 'a', newline='', encoding='utf-8') as fichier_csv:
+                writter = csv.writer(fichier_csv)
+                writter.writerow([title, universal_product_code, price_including_tax, price_excluding_tax, number_available, product_description, category, review_rating, image_url])
+
+scrap_books()
